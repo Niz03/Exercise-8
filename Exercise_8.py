7f7441f77c421fbbddb8bffa8b5b950c27c341fc
Nizar Arjoun <nArjoun@mcm.edu.ph>
2022-01-03 23:23:22 +0800

    Add project files.

---
+++ Exercise_8.py
@@ -0,0 +1,31 @@
+# Declaration of variables
+# pls send help im dying
+data = [5, 3, 7]
+print("Data = ", data)
+print("\n")
+
+# Bunch of print statements for task 1
+print("Task 1 Set")
+print("--------------------------------------------------")
+print(data[2])
+print(data[-1])
+print(len(data))
+print(data[0:2])
+print(0 in data)
+print(data + [2, 10, 5])
+print(tuple(data))
+print("\n")
+
+# Bnhc of print statements for task 2
+print("Task 2")
+print("--------------------------------------------------")
+data[0] = -data[0]
+print(data)
+data.append(10)
+print(data)
+data.insert(2, 22)
+print(data)
+data.pop(1)
+print(data)
+data.sort()
+print(data)
